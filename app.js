09365b925ecf2b0a40d984e2148c5ed80dbea527
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-05-24 14:27:46 +0100

    Pink div added

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('#container');
+
   const redText = document.createElement('p');
   redText.setAttribute('id', 'red');
   redText.textContent = 'Hey, I\'m red!';
   container.appendChild(redText);
+
+  const blueSubheading = document.createElement('h3');
+  blueSubheading.setAttribute('id', 'blue');
+  blueSubheading.textContent = 'I\'m a blue h3!';
+  container.appendChild(blueSubheading);
+
+  const pinkDiv = document.createElement('div');
+  pinkDiv.classList.add('pink-div');
+  container.appendChild(pinkDiv);
+  const pinkDivHeader = document.createElement('h1');
+  pinkDivHeader.textContent = 'I\'m in a div!';
+  pinkDiv.appendChild(pinkDivHeader);
+  pinkDivText = document.createElement('p');
+  pinkDivText.textContent = 'Me too!';
+  pinkDiv.appendChild(pinkDivText);
 });
